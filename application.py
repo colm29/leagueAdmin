d577056e4943e0307941300532f994448ffcc764
Colm Faherty <colm29@gmail.com>
2019-05-26 23:21:46 +0100

    FB initial login

--- application.py
+++ application.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python2
 import pdb
-from flask import Flask, render_template, url_for, flash, jsonify, request, redirect
+from flask import Flask, render_template, url_for, flash, jsonify, request, redirect,  make_response, session as login_session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db_setup import Base, Division, Team
+import httplib2
+import requests
+import json
+import random
+import string
 
 app = Flask(__name__)
+
+FBCLIENT_ID = json.loads(
+    open('fbclientsecrets.json', 'r').read())['web']['app_id']
 
 engine = create_engine('sqlite:///league.db')
 Base.metadata.bind = engine
@@ -64,7 +72,85 @@ def showDivisions():
     finally:
         session.close()
 
+@app.route('/login')
+def showLogin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    # return "The current session state is %s" % login_session['state']
+    return render_template('login.html')
 
+@app.route('/fbconnect', methods =['POST'])
+def fbconnect():
+    """ if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response """
+    #pdb.set_trace()
+    access_token = request.data
+    print "access token received %s " % access_token
+
+
+    app_id = json.loads(open('fbclientsecrets.json', 'r').read())[
+        'web']['app_id']
+    app_secret = json.loads(
+        open('fbclientsecrets.json', 'r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+        app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+
+    # Use token to get user info from API
+    userinfo_url = "https://graph.facebook.com/v3.3/me"
+    '''
+        Due to the formatting for the result from the server token exchange we have to
+        split the token first on commas and select the first index which gives us the key : value
+        for the server access token then we split it on colons to pull out the actual token value
+        and replace the remaining quotes with nothing so that it can be used directly in the graph
+        api calls
+    '''
+    token = result.split(',')[0].split(':')[1].replace('"', '')
+
+    url = 'https://graph.facebook.com/v3.3/me?access_token=%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    # print "url sent for API access:%s"% url
+    # print "API JSON result: %s" % result
+    data = json.loads(result)
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data["name"]
+    login_session['email'] = data["email"]
+    login_session['facebook_id'] = data["id"]
+
+    # The token must be stored in the login_session in order to properly logout
+    login_session['access_token'] = token
+
+    # Get user picture
+    url = 'https://graph.facebook.com/v3.3/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['picture'] = data["data"]["url"]
+
+    # see if user exists
+    """ user_id = getUserID(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session) 
+    login_session['user_id'] = user_id """
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+
+    flash("Now logged in as %s" % login_session['username'])
+    return output
 
 @app.route('/division/<int:division_id>')
 @app.route('/division/<int:division_id>/teams')
@@ -138,6 +224,9 @@ def deleteTeam(division_id,team_id):
         pass
     finally:
         session.close()
+
+
+    #user functions
 
 
 if __name__ == '__main__':
