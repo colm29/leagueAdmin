d0d7545e80280c2d9d3af9733cbcf057ea04d416
Colm Faherty <colm29@gmail.com>
2019-06-19 23:16:51 +0100

    new data setup

--- lotsofteams.py
+++ lotsofteams.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from db_setup import Division, Base, Team, User
 
-engine = create_engine('sqlite:///AFLeague.db')
+engine = create_engine('sqlite:///league.db')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
